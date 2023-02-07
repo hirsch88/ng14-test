0e8586a602bf3424fea8dd4bb24858070b31cfd1
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2023-02-07 14:38:42 +0100

    frist commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,43 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `<bal-app class="has-sticky-footer">
+    <header>
+      <!-- Header content -->
+    </header>
+    <main class="container py-large">
+      <bal-accordion openLabel="open" closeLabel="close">
+        <p class="py-medium">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem
+          dicta culpa quae pariatur dolorum? Debitis aspernatur cumque expedita
+          corrupti libero quisquam fuga, impedit repellendus distinctio
+          assumenda quasi nulla ratione dolorem. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Ducimus, ipsum possimus? Commodi nisi
+          facere cum harum nam. Sed, sapiente dolores error iste soluta quae
+          reprehenderit culpa explicabo quod molestiae quasi. Lorem ipsum dolor
+          sit amet consectetur adipisicing elit. Modi aliquam dolor obcaecati
+          sint. Deleniti voluptatum libero nemo exercitationem accusamus quasi
+          dignissimos aut consectetur maiores nam harum, voluptate hic
+          consequuntur commodi!
+        </p>
+      </bal-accordion>
+      <bal-accordion openLabel="open" closeLabel="close" class="pt-large">
+        <p class="py-medium">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem
+          dicta culpa quae pariatur dolorum? Debitis aspernatur cumque expedita
+          corrupti libero quisquam fuga, impedit repellendus distinctio
+          assumenda quasi nulla ratione dolorem. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Ducimus, ipsum possimus? Commodi nisi
+          facere cum harum nam. Sed, sapiente dolores error iste soluta quae
+          reprehenderit culpa explicabo quod molestiae quasi. Lorem ipsum dolor
+          sit amet consectetur adipisicing elit. Modi aliquam dolor obcaecati
+          sint. Deleniti voluptatum libero nemo exercitationem accusamus quasi
+          dignissimos aut consectetur maiores nam harum, voluptate hic
+          consequuntur commodi!
+        </p>
+      </bal-accordion>
+    </main>
+  </bal-app>`,
 })
 export class AppComponent {
   title = 'angular-14';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {
+  BalAccordionModule,
+  BalCoreModule,
+} from '@baloise/design-system-components-angular';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BalCoreModule.forRoot(),
+    BalAccordionModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}
